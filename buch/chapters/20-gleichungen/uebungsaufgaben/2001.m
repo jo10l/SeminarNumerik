4ae70e9c96389e81505f2bc3b868cb82038f157c
Andreas Müller <andreas.mueller@othello.ch>
2020-02-12 12:38:53 +0100

    Interpolation, Aufgabe 2001

---
+++ buch/chapters/20-gleichungen/uebungsaufgaben/2001.m
@@ -0,0 +1,17 @@
+#
+# 2001.m -- Nullstelle mit Newton findn
+#
+# (c) 2020 Prof Dr Andreas Müller, Hochschule Rapperswil
+#
+
+function retval = N(x)
+	retval = x - (x^5-(5/4)*x^4+1/4)/(5*(x-1)*x^3);
+endfunction
+
+x = 0.5;
+
+format long
+
+for i = (1:20)
+	x = N(x)
+endfor
